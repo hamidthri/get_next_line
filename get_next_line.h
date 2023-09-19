b626eb577b5210e836f283d084aa60ab1ef59eb5
Hamid Taheri <htaheri@3-B-1.42heilbronn.de>
2023-09-19 18:47:45 +0200

    separated version of GNL

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htaheri <htaheri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 18:55:16 by htaheri           #+#    #+#             */
+/*   Updated: 2023/05/09 19:54:53 by htaheri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char *buffer, char *temp);
+char	*ft_strchr(const char *s, int c);
+int		check(char *buffer, char *temp);
+#endif
